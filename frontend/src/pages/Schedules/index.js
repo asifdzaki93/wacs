4406302c2af158a67208c897517789e04350c1e6
asifdzaki93 <asifdzaki93@gmail.com>
2024-07-03 00:04:05 +0700

    bhs

--- frontend/src/pages/Schedules/index.js
+++ frontend/src/pages/Schedules/index.js
@@ -20,7 +20,7 @@ import { SocketContext } from "../../context/Socket/SocketContext";
 import { AuthContext } from "../../context/Auth/AuthContext";
 import usePlans from "../../hooks/usePlans";
 import { Calendar, momentLocalizer } from "react-big-calendar";
-import "moment/locale/pt-br";
+import "moment/locale/id-id";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
@@ -60,14 +60,7 @@ var defaultMessages = {
   noEventsInRange: "Tidak ada jadwal dalam periode ini.",
   showMore: function showMore(total) {
     return "+" + total + " lagi";
-  },
-  sunday: "Minggu",
-  monday: "Senin",
-  tuesday: "Selasa",
-  wednesday: "Rabu",
-  thursday: "Kamis",
-  friday: "Jumat",
-  saturday: "Sabtu"
+  }
 };
 
 const reducer = (state, action) => {
