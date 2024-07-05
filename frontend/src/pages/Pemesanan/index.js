302782fd632ab94cb63eb53f480eef16fe04907f
asifdzaki93 <asifdzaki93@gmail.com>
2024-07-05 08:07:04 +0700

    pemesanan

--- frontend/src/pages/Pemesanan/index.js
+++ frontend/src/pages/Pemesanan/index.js
@@ -1,20 +1,41 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(1),
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
   },
-}));
+});
 
 const Pemesanan = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <h1>Hello World</h1>
+    <div>
+      <h1>Dashboard Pemesanan</h1>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>No.</TableCell>
+              <TableCell align="right">Nama Pemesan</TableCell>
+              <TableCell align="right">Tanggal Pemesanan</TableCell>
+              <TableCell align="right">Total Harga</TableCell>
+              <TableCell align="right">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* Data pemesanan akan ditampilkan di sini */}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
