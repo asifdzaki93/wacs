1315aa3ff1c1935e2be98814ced13c1411e7c4fb
asifdzaki93 <asifdzaki93@gmail.com>
2024-07-06 01:37:31 +0700

    style

--- frontend/src/pages/Kasir/index.js
+++ frontend/src/pages/Kasir/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+
+    container: {
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.padding,
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(2),
+    }
+  }));
 
 const Kasir = () => {
+    const classes = useStyles();
   return (
     <div>
+    <Container maxWidth="lg" className={classes.container}>
+
       <h1>Hello World</h1>
-    </div>
+
+    </Container >
+    </div >
   );
 };
 
