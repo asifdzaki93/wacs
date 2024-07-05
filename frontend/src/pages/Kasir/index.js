a0a595152bcee163bd9cc4d4c3dd0e901d8dc5ec
asifdzaki93 <asifdzaki93@gmail.com>
2024-07-06 02:05:01 +0700

    tabel

--- frontend/src/pages/Kasir/index.js
+++ frontend/src/pages/Kasir/index.js
@@ -1,28 +1,70 @@
 import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
-import { Container } from "@material-ui/core"; // Tambahkan import Container dari Material-UI
+import { Container, TextField, Button, Table, TableBody, TableCell, TableHead, TableRow, IconButton } from "@material-ui/core"; // Tambahkan import Container, TextField, Button, Table, TableBody, TableCell, TableHead, TableRow, IconButton dari Material-UI
 
 const useStyles = makeStyles(theme => ({
-
     container: {
       paddingTop: theme.spacing(1),
-      paddingBottom: theme.padding,
+      paddingBottom: theme.spacing(2),
       paddingLeft: theme.spacing(1),
       paddingRight: theme.spacing(2),
-    }
+    },
+    table: {
+      minWidth: 700,
+    },
   }));
 
 const Kasir = () => {
     const classes = useStyles();
-  return (
-    <div>
-    <Container maxWidth="lg" className={classes.container}>
 
-      <h1>Hello World</h1>
-
-    </Container >
-    </div >
-  );
+    return (
+      <div>
+        <Container maxWidth="lg" className={classes.container}>
+          <TextField
+            id="search"
+            label="Cari"
+            type="search"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <Button variant="contained" color="primary" style={{ marginTop: '10px' }}>
+            Buat
+          </Button>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Penjualan ID</TableCell>
+                <TableCell align="right">Tanggal</TableCell>
+                <TableCell align="right">Total</TableCell>
+                <TableCell align="right">Aksi</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* Data penjualan akan ditampilkan di sini */}
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  1
+                </TableCell>
+                <TableCell align="right">2021-01-01</TableCell>
+                <TableCell align="right">Rp 100.000</TableCell>
+                <TableCell align="right">
+                  <IconButton aria-label="view">
+                    <ViewIcon />
+                  </IconButton>
+                  <IconButton aria-label="edit">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Container>
+      </div>
+    );
 };
 
 export default Kasir;
