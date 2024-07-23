47eb2fbf8dcc3c1aa50ca9caec75d25e6201a64b
asifdzaki93 <asifdzaki93@gmail.com>
2024-07-24 03:02:39 +0700

    map

--- backend/src/services/PlaceSearchService/PlaceSearchService.ts
+++ backend/src/services/PlaceSearchService/PlaceSearchService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiKey = 'AIzaSyAN01dly9kKxxb3IPZWJhcLbaD__dpZ-ts'; // Ganti dengan kunci API Anda
 
-const fetchPlaces = async (keyword, nextPageToken = '') => {
+const fetchPlaces = async (keyword: string, nextPageToken: string = '') => {
   const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(keyword)}&key=${apiKey}${nextPageToken ? `&pagetoken=${nextPageToken}` : ''}`;
 
   const response = await axios.get(url);
@@ -14,7 +14,7 @@ const fetchPlaces = async (keyword, nextPageToken = '') => {
   return response.data;
 };
 
-const getPlaceDetails = async (placeId) => {
+const getPlaceDetails = async (placeId: string) => {
   const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,formatted_phone_number,formatted_address&key=${apiKey}`;
   const response = await axios.get(url);
 
@@ -25,7 +25,7 @@ const getPlaceDetails = async (placeId) => {
   return response.data.result;
 };
 
-const PlaceSearchService = async (keyword) => {
+const PlaceSearchService = async (keyword: string) => {
   let results = [];
   let nextPageToken = '';
 
